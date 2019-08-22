17b1b68dade08a46c983fa2dbc4ecf6ab72add67
RileyBrodie <riley.brodie75@my.jcu.edu.au>
2019-08-22 12:59:18 +1000

    Change password length check to "<" system instead of range

--- password_entry.py
+++ password_entry.py
@@ -3,7 +3,7 @@ min_length = 2
 max_length = 10
 
 password = str(input("Enter a password"))
-while len(password) not in range(min_length, max_length + 1):
+while min_length > len(password) or len(password) > max_length:
     print("please enter a password between 2 and 10 characters")
     password = str(input("Enter a password"))
 
