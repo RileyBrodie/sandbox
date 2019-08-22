cb05053d0c1e7d4d7933fbe38c91a14f21d7e11b
RileyBrodie <riley.brodie75@my.jcu.edu.au>
2019-08-22 12:18:48 +1000

    Add error checking and password conversion

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,10 @@
 """Riley Brodie"""
+min_length = 2
+max_length = 10
+
+password = str(input("Enter a password"))
+while len(password) not in range(min_length, max_length + 1):
+    print("please enter a password between 2 and 10 characters")
+    password = str(input("Enter a password"))
+
+print("*" * len(password))
